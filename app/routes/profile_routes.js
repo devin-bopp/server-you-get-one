d8a5c15c9036057ed1bb3f088879b2f642543d11
devinrbopp <devinrbopp@gmail.com>
2022-01-09 20:27:45 -0700

    get and post routes for profile

--- app/routes/profile_routes.js
+++ app/routes/profile_routes.js
@@ -14,14 +14,22 @@ const requireToken = passport.authenticate('bearer', { session: false })
 
 const router = express.Router()
 
+// get current user's profile
 router.get('/profile', requireToken, (req, res, next) => {
-    res.status(200).json({message: 'something'})
-    // Profile.find()
-    //     .then(profile => {
-    //         return profile.toObject()
-    //     })
-    //     .then(profile => res.status(200).json({ message: "profile"}))
-    //     .catch(next)
+    Profile.find({ owner: req.user._id })
+        .then(profile => {
+            res.status(200).json(profile)
+        })
+        .catch(next)
+})
+
+// create a new profile for the current user
+router.post('/profile', requireToken, (req, res, next) => {
+    Profile.create(req.body)
+        .then(profile => {
+            res.status(201).json({ profile: profile.toObject() })
+        })
+        .catch(next)
 })
 
 module.exports = router
